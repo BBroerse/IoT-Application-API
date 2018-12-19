6df9cfb3d427fdc10e6a36eae117b59b28a26970
Bas Broerse <bas.broerse@hva.nl>
2018-12-19 20:34:20 +0100

    Added uuid-ossp extension to the migration

--- migrations/01_measurements.js
+++ migrations/01_measurements.js
@@ -1,30 +1,33 @@
 'use strict';
 
 exports.up = (knex, Promise) => {
-  return knex.schema
-    .createTable('measurements', (table) => {
-      table
-        .uuid('id')
-        .primary()
-        .notNullable()
-        .defaultsTo(knex.raw("uuid_generate_v4()"))
+  return Promise.all([
+    knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'),
 
-      table
-        .decimal('temperature')
-        .notNullable()
-
-      table
-        .decimal('humidity')
-        .notNullable()
-
-      table
-        .decimal('co2_ppm')
-        .notNullable()
+    knex.schema
+      .createTable('measurements', (table) => {
+        table
+          .uuid('id')
+          .primary()
+          .notNullable()
+          .defaultsTo(knex.raw("uuid_generate_v4()"))
 
         table
-          .timestamp('created_at').defaultTo(knex.fn.now());
-    })
+          .decimal('temperature')
+          .notNullable()
 
+        table
+          .decimal('humidity')
+          .notNullable()
+
+        table
+          .decimal('co2_ppm')
+          .notNullable()
+
+          table
+            .timestamp('created_at').defaultTo(knex.fn.now());
+      })
+  ])
 };
 
 exports.down = function (knex, Promise) {
